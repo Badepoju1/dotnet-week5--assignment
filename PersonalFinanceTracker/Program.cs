805d261daa8321a196a79fb844c70370e0f0a945
Badepoju1 <122815832+Badepoju1@users.noreply.github.com>
2026-03-19 08:29:57 +0100

    creation of the program.cs file containing the program class and Main block

---
+++ PersonalFinanceTracker/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Assemblies;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+namespace PersonalFinanceTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FinanceTracker tracker = new FinanceTracker()!; // creating a new object from the class
+
+            Console.Write("Enter Your Name: ");
+            tracker.UserName = CheckField(Console.ReadLine());
+
+            int transactionID = 1;
+
+            static string CheckField(string? field) // a class method to validate client input
+            {
+                while (string.IsNullOrEmpty(field))
+                {
+                    Console.WriteLine("field cannot be empty or null");
+                    Console.Write("Enter Appropriate Value: ");
+                    field = Console.ReadLine()!; // ask again
+                }
+                return field;
+                
+            }
+
+            static decimal DecimalNum(decimal input) // a class method to validate client input
+            {
+                while (input <= 0)
+                {
+                    Console.WriteLine("Value cannot be negative or zero");
+                    Console.Write("Enter Tranction Amount: ");
+                    input = Convert.ToDecimal(Console.ReadLine()); // ask again
+                }
+                return input;
+            }
+
+            static int IntNum(int opt) // a class method to validate client input
+            {
+                while (opt <= 0 || opt >= 10)
+                {   
+                    
+                    Console.WriteLine("Invalid input, option should be 1 - 9");
+                    Console.Write("Input a valid option: ");
+                    opt = Convert.ToInt32(Console.ReadLine()); // ask again
+                    
+                }
+                return opt;
+            }
+
+            
+            bool running = true;
+            while (running)
+            {
+                try
+                {   Console.Clear();
+                    Console.WriteLine("\n=== Personal Finance Tracker ===");
+                    Console.WriteLine($"Welcome, {tracker.UserName}!");
+                    Console.WriteLine($"Current Balance: ${tracker.CurrentBalance}");
+                    Console.WriteLine("");
+
+                    Console.WriteLine("\n1. Add Income");
+                    Console.WriteLine("2. Add Expense");
+                    Console.WriteLine("3. View All Transactions");
+                    Console.WriteLine("4. View Income Only");
+                    Console.WriteLine("5. View Expenses Only");
+                    Console.WriteLine("6. View by Category");
+                    Console.WriteLine("7. View Highest Expense");
+                    Console.WriteLine("8. Display Monthly Summary");
+                    Console.WriteLine("9. Exit");
+
+                    Console.Write("\nSelect an option: ");
+                    int option = IntNum(Convert.ToInt32(Console.ReadLine())); // Read user option and convert to integer
+                    
+                    // Exit option
+                    if (option == 9)
+                    {
+                        Console.WriteLine("Program is successfully terminated. Thank you."); // Friendly termination message
+                        break;              // Exit the while loop
+                    }
+
+                    switch (option)
+                    {
+                        case 1: // case one and case 2 uses the same code but I use the shorthand for loop to differentiate the action upon the option selected
+                        case 2:
+
+                            Console.Write("Enter Transaction Description: ");
+                            string desc = CheckField(Console.ReadLine()); // calling a defined method
+                            
+
+                            Console.Write("Enter Tranction Amount: ");
+                            decimal amount = DecimalNum(Convert.ToDecimal(Console.ReadLine())); // calling a defined method
+
+                            Console.WriteLine("Category: Salary, Food, Transport, Entertainment, Bills, Gift");
+                            Console.Write("Enter Category: ");
+                            string? category = CheckField(Console.ReadLine()); // calling a defined method
+
+                            string type = option == 1 ? "Income" : "Expense"; // shorthand method to determine the case action and differentiate their record
+
+                            Transaction t = new Transaction(transactionID++, desc, amount, type, category);
+                            tracker.AddTransaction(t);
+
+                            Console.WriteLine("Transaction added successfully!");
+                            break;
+
+                        case 3:
+                            tracker.DisplayAllTransactions(); // calling a defined method
+                            break;
+
+                        case 4:
+                            tracker.DisplayIncomeTransactions(); // calling a defined method
+                            break;
+
+                        case 5:
+                            tracker.DisplayExpenseTransactions(); // calling a defined method
+                            break;
+
+                        case 6:
+                            Console.Write("Enter category: ");
+                            string cat = CheckField(Console.ReadLine());
+                            tracker.DisplayTransactionsByCategory(cat); // calling a defined method
+                            break;
+
+                        case 7:
+                            var highest = tracker.GetHighestExpense(); // calling a defined method
+                            if (highest != null)
+                                Console.WriteLine($"Highest Expense: {highest.Category} - ${highest.Amount}");
+                            break;
+
+                        case 8:
+                            tracker.DisplayMonthlySummary();
+                            break;
+
+                    }
+                }
+                catch (System.Exception)
+                {
+                    
+                    Console.WriteLine("Invalid input, please enter appropriate value");
+                }
+                
+            }
+        }
+
+    }
+}
+
